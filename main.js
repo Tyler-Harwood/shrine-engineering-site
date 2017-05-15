7539e4d1b2ec25b85f3afcccb818e0c97f84ea1c
Tyler-Harwood <andrewtharwood@gmail.com>
2017-05-15 10:08:33 -0400

    Got subscribe working, added new emoji, added credit to twemoji, updated footor coloring and size, updated link coloring, added social media links, removed some excess stuff from About

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 <!--New images on load -->
-var images = ['1f404.svg', '1f31d.svg', '1f31e.svg', '1f404.svg', '1f40b.svg', '1f68c.svg', '1f69b.svg', '1f6b4.svg'];
-$('<img id="titleEmoji" src=images/' + images[Math.floor(Math.random() * images.length)] + '>').appendTo('#title');
+var images = ['1f1fa-1f1f8.svg', '1f31e.svg', '1f354.svg', '1f363.svg', '1f402.svg', '1f404.svg', '1f406.svg', '1f40a.svg',
+    '1f410.svg', '1f466.svg', '1f468.svg', '1f472.svg', '1f5ff.svg', '1f69b.svg', '1f6b4.svg', '1f31d.svg',
+    '1f344.svg', '1f355.svg', '1f38e.svg', '1f403.svg', '1f405.svg', '1f407.svg', '1f40b.svg', '1f419.svg', '1f467.svg',
+    '1f46f.svg', '1f479.svg', '1f47e.svg', '1f68c.svg', '1f69e.svg', '1f6b6.svg'];
+$('<img id="titleEmoji" src=images/twemoji/' + images[Math.floor(Math.random() * images.length)] + '>').appendTo('#title');
 
 $(document).ready(function () {
-
-    for (var i = 1; i < 6; i++) {
-        var target = '.growAnimation' + i;
-        jQuery(target).viewportChecker({
-            classToAdd: 'animated',
-            offset: 10
-        });
-    }
-
+    $("#city").load("city.html", function () {
+            for (var i = 1; i < 6; i++) {
+                var target = '.growAnimation' + i;
+                jQuery(target).viewportChecker({
+                    classToAdd: 'animated',
+                    offset: 10
+                });
+            }
+            $(".soc").load("socialMedia.html")
+        }
+    );
 
     $('.form_error').hide();
     $('#fireworks').hide();
@@ -25,33 +30,57 @@ $(document).ready(function () {
         if (email === '') {
             $email.next().show();
             $invalidEmail.text("sorry, but we can't subscribe you to the email list without an email :c");
-            $emailField.css('border-color', "red")
+            $emailField.css('border-color', "red");
+            $invalidEmail.css('color', "orange");
             $invalidEmail.show();
             return false;
         }
+
         if (IsEmail(email) === false) {
             $email.next().show();
             $invalidEmail.text("You sure that's a valid email? The computer thinks it looks a little off...");
-            $emailField.css('border-color', "red")
+            $emailField.css('border-color', "red");
+            $invalidEmail.css('color', "orange");
             $invalidEmail.show();
             return false;
         }
+
+        // var captchaResponse = grecaptcha.getResponse();
+        //
+        // if (captchaResponse ==='') {
+        //     $email.next().show();
+        //     $invalidEmail.text("Please complete the captcha below to help protect us from bots!");
+        //     $emailField.css('border-color', "orange");
+        //     $invalidEmail.css('color', "yellow");
+        //     $invalidEmail.show();
+        //     return false;
+        // }
+
         var data = JSON.stringify({
-            "captchaResponse": grecaptcha.getResponse(),
+            // "captchaResponse": captchaResponse,
             "email": email
         });
 
+        $invalidEmail.hide();
         $('#loader').show();
         console.log(data);
         //ajax call php page
         $.post("https://er778qqu6h.execute-api.us-east-1.amazonaws.com/dev/subscribe", data, function (response) {
+            console.log(response);
+            if (response.success) {
+                $invalidEmail.text(response.message);
+                $invalidEmail.css('color', "#e6fdf1");
+                $invalidEmail.show();
+                if (response.message.startsWith("We just sent you")) {
+                    $('#fireworks').show();
+                }
+            }
             $('#loader').hide();
-            $('#fireworks').show();
         }, "json");
         return false;
     });
 
-    $("#emailField").bind('input',function () {
+    $("#emailField").bind('input', function () {
         $("#invalid_email").hide();
         $("#emailField").css('border-color', "")
     });
